4bb55944d0a04c147aa39ef0b330d296d7475db8
Piotr Krzeminski <git@krzeminski.it>
2020-09-24 21:52:47 +0200

    Add another frontend route

--- src/jsMain/kotlin/client.kt
+++ src/jsMain/kotlin/client.kt
@@ -1,13 +1,30 @@
 import react.dom.render
 import kotlinx.browser.document
 import kotlinx.browser.window
+import react.router.dom.browserRouter
+import react.router.dom.route
+import react.router.dom.switch
 
 fun main() {
     window.onload = {
         render(document.getElementById("root")) {
-            child(Welcome::class) {
-                attrs {
-                    name = "Kotlin/JS"
+            browserRouter {
+                switch {
+                    route("/", exact = true) {
+                        child(Welcome::class) {
+                            attrs {
+                                name = "Root page"
+                            }
+                        }
+                    }
+                    route("/zoos") {
+                        // TODO fetch zoos from API
+                        child(Welcome::class) {
+                            attrs {
+                                name = "Zoos"
+                            }
+                        }
+                    }
                 }
             }
         }
--- src/jvmMain/kotlin/server.kt
+++ src/jvmMain/kotlin/server.kt
@@ -1,20 +1,24 @@
 import io.ktor.application.*
 import io.ktor.html.respondHtml
 import io.ktor.http.HttpStatusCode
-import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.util.pipeline.*
 import kotlinx.html.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         routing {
-            get("/") {
-                webApp()
+            get("/") { webApp() }
+            get("/zoos") { webApp() }
+            route("/api") {
+                get("/zoos") {
+                    call.respond("Some zoos")
+                }
             }
             static("/static") {
                 resources()
