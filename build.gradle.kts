018b84cf8912e80235bae379d8b0a0b2ef67c875
Piotr Krzeminski <git@krzeminski.it>
2021-01-30 20:13:00 +0100

    Bump version to 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
     // Having this version in one place keeps version of the library in sync, both what's exposed and what's consumed by
     // the end-to-end tests.
-    val kotlinRpcVersion by extra { "0.1.2" }
+    val kotlinRpcVersion by extra { "0.1.3" }
 }
